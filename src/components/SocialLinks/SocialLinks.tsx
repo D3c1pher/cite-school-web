8f519adfb09aa52cc96b7be46bc809e5d30e43a4
D3c1pher <norven10@gmail.com>
2025-05-14 14:02:31 +0800

    style: changed border and rotate

--- src/components/SocialLinks/SocialLinks.tsx
+++ src/components/SocialLinks/SocialLinks.tsx
@@ -4,16 +4,16 @@ import { faFacebookF, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
 
 function SocialLinks() {
   return (
-    <ul className="flex gap-4">
+    <ul className="flex gap-2">
       <li>
         <Link
           href="https://www.facebook.com/citeinfo"
           target="_blank"
           rel="noopener noreferrer"
           aria-label="Facebook"
-          className="group relative flex h-12 w-12 items-center justify-center overflow-hidden rounded-full border-4 border-white bg-white"
+          className="group relative flex h-12 w-12 items-center justify-center overflow-hidden rounded-full border-3 border-white bg-white"
         >
-          <i className="text-primary-dark z-10 text-2xl transition-transform duration-500 group-hover:rotate-y-[360deg] group-hover:text-white">
+          <i className="text-primary-dark z-10 text-2xl transition-transform duration-500 group-hover:rotate-y-360 group-hover:text-white">
             <FontAwesomeIcon icon={faFacebookF} />
           </i>
           <span className="bg-facebook absolute top-full left-0 z-0 h-full w-full transition-all duration-500 group-hover:top-0"></span>
@@ -25,9 +25,9 @@ function SocialLinks() {
           target="_blank"
           rel="noopener noreferrer"
           aria-label="LinkedIn"
-          className="group relative flex h-12 w-12 items-center justify-center overflow-hidden rounded-full border-4 border-white bg-white"
+          className="group relative flex h-12 w-12 items-center justify-center overflow-hidden rounded-full border-3 border-white bg-white"
         >
-          <i className="text-primary-dark z-10 text-2xl transition-transform duration-500 group-hover:rotate-y-[360deg] group-hover:text-white">
+          <i className="text-primary-dark z-10 text-2xl transition-transform duration-500 group-hover:rotate-y-360 group-hover:text-white">
             <FontAwesomeIcon icon={faLinkedinIn} />
           </i>
           <span className="bg-linkedin absolute top-full left-0 z-0 h-full w-full transition-all duration-500 group-hover:top-0"></span>
