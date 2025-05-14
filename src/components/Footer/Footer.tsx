8f7aa9dce948075604c2f5f1adc74090e87fee9b
D3c1pher <norven10@gmail.com>
2025-05-14 13:58:35 +0800

    refactor: made social links as new component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,21 +3,26 @@
 import React from "react";
 import Link from "next/link";
 import { navLinks } from "@/data";
+import SocialLinks from "../SocialLinks/SocialLinks";
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer>
-      <div>
+    <footer className="from-primary to-primary-light w-full bg-gradient-to-r text-white">
+      <div className="container mx-auto flex flex-wrap justify-around md:flex-row-reverse">
         {/* Logo Image */}
-        <Link href="/">
-          <img src="/images/cite-logo-text-wht.png" alt="CITE Logo" />
+        <Link href="/" className="flex items-center">
+          <img
+            src="/images/cite-logo-text-wht.png"
+            alt="CITE Logo"
+            className="pointer-events-none h-24 w-auto select-none md:h-28 lg:h-32"
+          />
         </Link>
 
-        <div>
+        <div className="flex flex-wrap justify-around">
           {/* Contact Information */}
-          <div>
+          <div className="max-w-sm">
             <h2>Contact Information</h2>
             <div>
               <p>CITE Technical Institute, Inc.</p>
@@ -37,7 +42,7 @@ const Footer = () => {
           </div>
 
           {/* Links */}
-          <div>
+          <div className="max-w-sm">
             {/* Navigation Links */}
             <div>
               <h2>Navigation Links</h2>
@@ -56,31 +61,14 @@ const Footer = () => {
             {/* Social Links */}
             <div>
               <h2>Follow Us</h2>
-              <div>
-                <a
-                  href="https://www.facebook.com/citeinfo"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  aria-label="Facebook"
-                >
-                  <div>fb placeholder</div>
-                </a>
-                <a
-                  href="https://www.linkedin.com/in/cite-technical-institute-inc-7bb018258/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  aria-label="LinkedIn"
-                >
-                  <div>linkedin placeholder</div>
-                </a>
-              </div>
+              <SocialLinks />
             </div>
           </div>
         </div>
       </div>
 
-      {/* Legal Links & Copyright */}
-      <div>
+      {/* Copyright */}
+      <div className="bg-primary-dark text-center">
         <p>
           © {currentYear} CITE Technical Institute, Inc. All Rights Reserved.
         </p>
---
+++ src/components/SocialLinks/SocialLinks.tsx
@@ -0,0 +1,40 @@
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebookF, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
+
+function SocialLinks() {
+  return (
+    <ul className="flex gap-4">
+      <li>
+        <Link
+          href="https://www.facebook.com/citeinfo"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Facebook"
+          className="group relative flex h-12 w-12 items-center justify-center overflow-hidden rounded-full border-4 border-white bg-white"
+        >
+          <i className="text-primary-dark z-10 text-2xl transition-transform duration-500 group-hover:rotate-y-[360deg] group-hover:text-white">
+            <FontAwesomeIcon icon={faFacebookF} />
+          </i>
+          <span className="bg-facebook absolute top-full left-0 z-0 h-full w-full transition-all duration-500 group-hover:top-0"></span>
+        </Link>
+      </li>
+      <li>
+        <Link
+          href="https://www.linkedin.com/in/cite-technical-institute-inc-7bb018258/"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="LinkedIn"
+          className="group relative flex h-12 w-12 items-center justify-center overflow-hidden rounded-full border-4 border-white bg-white"
+        >
+          <i className="text-primary-dark z-10 text-2xl transition-transform duration-500 group-hover:rotate-y-[360deg] group-hover:text-white">
+            <FontAwesomeIcon icon={faLinkedinIn} />
+          </i>
+          <span className="bg-linkedin absolute top-full left-0 z-0 h-full w-full transition-all duration-500 group-hover:top-0"></span>
+        </Link>
+      </li>
+    </ul>
+  );
+}
+
+export default SocialLinks;
